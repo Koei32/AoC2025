bcff27d942cf4f7cb4f0993042c343db5d5683ec
Koei <eshanshekhaji@gmail.com>
2025-12-22 22:42:42 +0530

    Day 1 - use of ints instead of floats

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -15,29 +15,29 @@ fn main() {
     let inputs: Vec<String> = reader.lines().map(|x| x.unwrap()).collect();
 
     // dial starts at 50
-    let mut dial: f32 = 50.0;
+    let mut dial: i32 = 50;
 
     // password is number of times the dial is at zero
     let mut pwd: i32 = 0;
 
     for input in inputs {
         // number of clicks
-        let clicks: f32 = input.get(1..).unwrap().parse::<f32>().unwrap();
+        let clicks: i32 = input.get(1..).unwrap().parse::<i32>().unwrap();
 
         // direction of rotation (1 is right, -1 is left)
-        let direction: f32 = match input.get(0..1) {
-            Some("R") => 1.0,
-            Some("L") => -1.0,
-            _ => 0.0,
+        let direction: i32 = match input.get(0..1) {
+            Some("R") => 1,
+            Some("L") => -1,
+            _ => 0,
         };
 
         // target after this rotation
-        let next: f32 = clicks.mul_add(direction, dial);
+        let next: i32 = clicks + (direction * dial);
 
         // rotating the dial and checking every click (may not be the most optimal technique)
         while dial as i32 != next as i32 {
             dial += direction;
-            if dial % 100.0 == 0.0 {
+            if dial % 100 == 0 {
                 pwd += 1;
             }
         }
